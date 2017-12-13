4711be6cbc71dd344ecc872a306133edfd072d95
zi-kang <hzkdeyx@126.com>
2017-12-13 23:47:56 +0800

    初次接触buffer

---
+++ buffer.js
@@ -0,0 +1,13 @@
+let buf = Buffer.alloc(256);
+// let len = buf.write("测试buffer写入字符数");
+//
+// console.log("写入字节数 : "+  len);
+
+for (let i = 0 ; i < 26 ; i++) {
+    buf[i] = i + 97;
+}
+
+console.log( buf.toString('ascii'));       // 输出: abcdefghijklmnopqrstuvwxyz
+console.log( buf.toString('ascii',0,5));   // 输出: abcde
+console.log( buf.toString('utf8',0,5));    // 输出: abcde
+console.log( buf.toString(undefined,0,5)); // 使用 'utf8' 编码, 并输出: abcde
